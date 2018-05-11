d1dd25c55aabc2b7ba83afe2dd04e5be09567ca0
Bartosz Lorek <bery.lorek@gmail.com>
2018-05-11 03:29:19 +0200

    api for requests set

---
+++ src/.internal/create-requests.js
@@ -0,0 +1,37 @@
+function createRequests(set = []) {
+    return {
+        add: (request, name) => {
+            request.name = name || ''
+            set.push(request)
+            return true
+        },
+        remove: (request, iteratee) => {
+            let index = -1
+            const length = set.length
+            const name = typeof request === 'string' ? request : request.name
+
+            while (++index < length) {
+                if (set[index].name === name) {
+                    if (iteratee != null) {
+                        iteratee(set[index])
+                    }
+                    set.splice(index, 1)
+                    return false
+                }
+            }
+            return false
+        },
+        clear: iteratee => {
+            let index = set.length
+            while (index--) {
+                if (iteratee != null) {
+                    iteratee(set[index])
+                }
+                set.pop()
+            }
+            return false
+        }
+    }
+}
+
+export default createRequests
