5dbae41418c39d6d510be008d70deaaad2aad1ba
Bartosz Lorek <bery.lorek@gmail.com>
2018-05-12 22:40:55 +0200

    cover more cases

--- test/aframe.test.js
+++ test/aframe.test.js
@@ -4,15 +4,9 @@ const expectToBeInRange = (value, a, b) => {
     expect(value).toBeGreaterThanOrEqual(a)
     expect(value).toBeLessThanOrEqual(b)
 }
-
 const createDateNow = (now = 0) => {
     return () => now++
 }
-
-beforeEach(() => {
-    global.Date.now = createDateNow()
-    jest.clearAllTimers()
-})
 
 global.requestAnimationFrame = callback => {
     return setTimeout(callback, 1)
@@ -20,12 +14,22 @@ global.requestAnimationFrame = callback => {
 global.cancelAnimationFrame = requestId => {
     clearTimeout(requestId)
 }
-
 jest.useFakeTimers()
 
+beforeEach(() => {
+    global.Date.now = createDateNow()
+    jest.clearAllTimers()
+})
+
 describe('repeatUntil', () => {
+    it('should handle errors', () => {
+        const request = aframe.setTaskout()
+        expect(request).toEqual({ id: -1 })
+        jest.runTimersToTime(100)
+    })
+
     it('should return request object', () => {
-        const callback = jest.fn(() => false)
+        const callback = jest.fn()
         const request = repeatUntil(callback)
 
         expect(request).toEqual(
@@ -33,18 +37,8 @@ describe('repeatUntil', () => {
                 id: expect.any(Number)
             })
         )
-        jest.runTimersToTime(20)
-        const { calls } = callback.mock
-        expect(calls.length).toBe(1)
-    })
-
-    it('should call 20 times', () => {
-        const callback = jest.fn()
-        const request = repeatUntil(callback)
-
-        jest.runTimersToTime(20)
-        const { calls } = callback.mock
-        expect(calls.length).toBe(20)
+        jest.runTimersToTime(1)
+        expect(callback).toHaveBeenCalledTimes(1)
     })
 
     it('should stop after falsy output', () => {
@@ -52,41 +46,32 @@ describe('repeatUntil', () => {
         const callback = jest.fn(() => --limit > 0)
         const request = repeatUntil(callback)
 
-        jest.runAllTimers()
-        const { calls } = callback.mock
-        expect(calls.length).toBe(10)
+        jest.runTimersToTime(100)
+        expect(callback).toHaveBeenCalledTimes(10)
+    })
+
+    it('should call 20 times', () => {
+        const callback = jest.fn()
+        const request = repeatUntil(callback)
+
+        jest.runTimersToTime(20)
+        expect(callback).toHaveBeenCalledTimes(20)
     })
 })
 
 describe('repeatDelay', () => {
-    it('handle errors with falsy values', () => {
-        expect(() => repeatDelay()).not.toThrow()
-        expect(() => repeatDelay(null)).not.toThrow()
-
-        const callback = jest.fn()
-        expect(() => repeatDelay(callback)).not.toThrow()
-        expect(callback).toHaveBeenCalledTimes(0)
+    it('should handle errors', () => {
+        expect(repeatDelay()).toEqual({ id: -1 })
+        expect(repeatDelay(null)).toEqual({ id: -1 })
+        jest.runTimersToTime(100)
     })
 
-    it('should call 10 times (every 20 sec)', () => {
+    it('should assign 0 to the falsy delay', () => {
         const callback = jest.fn()
-        const request = repeatDelay(callback, 20)
+        repeatDelay(callback)
 
-        jest.runTimersToTime(200)
-        const { calls } = callback.mock
-        expect(calls.length).toBe(10)
-    })
-
-    it('should handle extra parameters', () => {
-        const callback = jest.fn()
-        const request = repeatDelay(callback, 20, 'a', 'b')
-
-        jest.runTimersToTime(200)
-        const { calls } = callback.mock
-        expect(calls.length).toBe(10)
-        expect(calls[0]).toEqual(['a', 'b'])
-        expect(calls[4]).toEqual(['a', 'b'])
-        expect(calls[8]).toEqual(['a', 'b'])
+        jest.runTimersToTime(100)
+        expect(callback).toHaveBeenCalledTimes(100)
     })
 
     it('should stop after falsy output', () => {
@@ -94,31 +79,58 @@ describe('repeatDelay', () => {
         const callback = jest.fn(() => --limit > 0)
         const request = repeatDelay(callback, 20)
 
-        jest.runAllTimers()
-        const { calls } = callback.mock
-        expect(calls.length).toBe(5)
+        jest.runTimersToTime(200) // 100 frames more
+        expect(callback).toHaveBeenCalledTimes(5)
+    })
+
+    it('should call 10 times (every 20 sec)', () => {
+        const callback = jest.fn()
+        const request = repeatDelay(callback, 20)
+
+        jest.runTimersToTime(200)
+        expect(callback).toHaveBeenCalledTimes(10)
+    })
+
+    it('should handle extra parameters', () => {
+        const callback = jest.fn()
+        const request = repeatDelay(callback, 20, 'a', 'b')
+
+        jest.runTimersToTime(200)
+        expect(callback).toHaveBeenCalledTimes(10)
+        expect(callback).toHaveBeenCalledWith('a', 'b')
     })
 })
 
 describe('api.setTimeout', () => {
-    it('handle errors with falsy values', () => {
-        expect(() => aframe.setTimeout()).not.toThrow()
-        expect(() => aframe.setTimeout(null)).not.toThrow()
-
-        const callback = jest.fn()
-        expect(() => aframe.setTimeout(callback)).not.toThrow()
-        expect(callback).toHaveBeenCalledTimes(0)
+    it('should handle errors', () => {
+        expect(aframe.setTimeout()).toEqual({ id: -1 })
+        expect(aframe.setTimeout(null)).toEqual({ id: -1 })
+        jest.runTimersToTime(100)
     })
 
-    it('should call after 20 ms', () => {
+    it('should assign 0 to the falsy delay', () => {
+        const callback = jest.fn()
+        const request = aframe.setTimeout(callback)
+        expect(request).not.toEqual({ id: -1 })
+        jest.runTimersToTime(100)
+    })
+
+    it('should call only once', () => {
+        const callback = jest.fn()
+        aframe.setTimeout(callback)
+
+        jest.runTimersToTime(100)
+        expect(callback).toHaveBeenCalledTimes(1)
+    })
+
+    it('should stop timer after 20 ms', () => {
         let counter = 0
         const timer = setInterval(() => ++counter, 1)
         const callback = jest.fn(() => clearInterval(timer))
         const request = aframe.setTimeout(callback, 20)
 
         jest.runTimersToTime(100)
-        const { calls } = callback.mock
-        expect(calls.length).toBe(1)
+        expect(callback).toHaveBeenCalledTimes(1)
         expect(counter).toBe(20)
     })
 
@@ -126,15 +138,14 @@ describe('api.setTimeout', () => {
         const callback = jest.fn()
         const request = aframe.setTimeout(callback, 5, 'a', 'b')
 
-        jest.runAllTimers()
-        const { calls } = callback.mock
-        expect(calls.length).toBe(1)
-        expect(calls[0]).toEqual(['a', 'b'])
+        jest.runTimersToTime(100)
+        expect(callback).toHaveBeenCalledTimes(1)
+        expect(callback).toHaveBeenCalledWith('a', 'b')
     })
 })
 
 describe('api.clear', () => {
-    it('handle errors with falsy values', () => {
+    it('should handle falsy values', () => {
         expect(() => aframe.clear()).not.toThrow()
         expect(() => aframe.clear(null)).not.toThrow()
         expect(() => aframe.clear({})).not.toThrow()
@@ -146,20 +157,40 @@ describe('api.clear', () => {
         const request = aframe.setInterval(callback, 10)
         setTimeout(() => aframe.clear(request), 50)
 
-        jest.runTimersToTime(200)
-        const { calls } = callback.mock
-        expect(calls.length).toBe(4)
+        jest.runTimersToTime(150) // 100 frames more
+        expect(callback).toHaveBeenCalledTimes(4)
+    })
+
+    it('should assign -1 to the request id', () => {
+        const request = { id: 10 }
+        aframe.clear(request)
+        expect(request).toEqual({ id: -1 })
     })
 })
 
 describe('api.setTaskout', () => {
-    it('handle errors with falsy values', () => {
-        expect(() => aframe.setTaskout()).not.toThrow()
-        expect(() => aframe.setTaskout(null)).not.toThrow()
+    it('should handle errors', () => {
+        expect(aframe.setTaskout()).toEqual({ id: -1 })
+        expect(aframe.setTaskout(null)).toEqual({ id: -1 })
+        jest.runTimersToTime(100)
+    })
 
+    it('should assign 0 to the falsy duration', () => {
         const callback = jest.fn()
-        expect(() => aframe.setTaskout(callback)).not.toThrow()
-        expect(callback).toHaveBeenCalledTimes(0)
+        aframe.setTaskout(callback)
+
+        jest.runTimersToTime(200)
+        expect(callback).toHaveBeenCalledTimes(1)
+        expect(callback).toHaveBeenCalledWith(1)
+    })
+
+    it('should assign 1 to the falsy steps', () => {
+        const callback = jest.fn()
+        aframe.setTaskout(callback, 10)
+
+        jest.runTimersToTime(200)
+        expect(callback).toHaveBeenCalledTimes(1)
+        expect(callback).toHaveBeenCalledWith(1)
     })
 
     it('should call 4 times with progress', () => {
@@ -198,20 +229,33 @@ describe('api.setTaskout', () => {
         const callback = jest.fn()
         const request = aframe.setTaskout(callback, 10, 2, 'a', 'b')
 
-        jest.runAllTimers()
+        jest.runTimersToTime(200)
         const { calls } = callback.mock
         expect(calls).toEqual([[0.5, 'a', 'b'], [1, 'a', 'b']])
     })
 })
 
 describe('api.setRandval', () => {
-    it('handle errors with falsy values', () => {
-        expect(() => aframe.setRandval()).not.toThrow()
-        expect(() => aframe.setRandval(null)).not.toThrow()
+    it('should handle errors', () => {
+        expect(aframe.setRandval()).toEqual({ id: -1 })
+        expect(aframe.setRandval(null)).toEqual({ id: -1 })
+        jest.runTimersToTime(100)
+    })
 
+    it('should assign 0 to the falsy delay', () => {
         const callback = jest.fn()
-        expect(() => aframe.setRandval(callback)).not.toThrow()
-        expect(callback).toHaveBeenCalledTimes(0)
+        aframe.setRandval(callback)
+
+        jest.runTimersToTime(100)
+        expect(callback).toHaveBeenCalledTimes(100)
+    })
+
+    it('should assign 0 to the falsy variation', () => {
+        const callback = jest.fn()
+        aframe.setRandval(callback, 10)
+
+        jest.runTimersToTime(100)
+        expect(callback).toHaveBeenCalledTimes(100)
     })
 
     it('should call 4 times with different delays', () => {
@@ -221,6 +265,7 @@ describe('api.setRandval', () => {
         jest.runTimersToTime(200)
         // min: 200 / (50 + 10) = ceil(3.33) - 1 = 3
         // max: 200 / (50 - 10) = 5 - 1 = 4
+        // x-1 because callback is after conditional
         const { calls } = callback.mock
         expectToBeInRange(calls.length, 3, 4)
     })
@@ -232,6 +277,7 @@ describe('api.setRandval', () => {
 
         // min: 50 / (20 + 5) = 2 - 1 = 1
         // max: 50 / (20 - 5) = ceil(3.33) - 1 = 3
+        // x-1 because callback is after conditional
         jest.runTimersToTime(200)
         const { calls } = callback.mock
         expectToBeInRange(calls.length, 1, 3)
