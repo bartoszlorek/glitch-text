ae6635c880892624da0e7c1138f44cd431346340
Bartosz Lorek <bery.lorek@gmail.com>
2018-05-13 02:28:14 +0200

    cover waitTimeout and clean tests

--- test/aframe.test.js
+++ test/aframe.test.js
@@ -101,6 +101,30 @@ describe('repeatDelay', () => {
     })
 })
 
+describe('api.clear', () => {
+    it('should handle falsy values', () => {
+        expect(() => aframe.clear()).not.toThrow()
+        expect(() => aframe.clear(null)).not.toThrow()
+        expect(() => aframe.clear({})).not.toThrow()
+        expect(() => aframe.clear(20)).not.toThrow()
+    })
+
+    it('should stop interval after 50 ms (inclusive)', () => {
+        const callback = jest.fn()
+        const request = aframe.setInterval(callback, 10)
+        setTimeout(() => aframe.clear(request), 50)
+
+        jest.runTimersToTime(150) // 100 frames more
+        expect(callback).toHaveBeenCalledTimes(4)
+    })
+
+    it('should assign -1 to the request id', () => {
+        const request = { id: 10 }
+        aframe.clear(request)
+        expect(request).toEqual({ id: -1 })
+    })
+})
+
 describe('api.setTimeout', () => {
     it('should handle errors', () => {
         expect(aframe.setTimeout()).toEqual({ id: -1 })
@@ -144,27 +168,71 @@ describe('api.setTimeout', () => {
     })
 })
 
-describe('api.clear', () => {
-    it('should handle falsy values', () => {
-        expect(() => aframe.clear()).not.toThrow()
-        expect(() => aframe.clear(null)).not.toThrow()
-        expect(() => aframe.clear({})).not.toThrow()
-        expect(() => aframe.clear(20)).not.toThrow()
+describe('api.waitTimeout', () => {
+    it('should handle errors', () => {
+        const falsy = expect.objectContaining({
+            waitTimeout: expect.any(Function),
+            id: -1
+        })
+        expect(aframe.waitTimeout()).toEqual(falsy)
+        expect(aframe.waitTimeout(null)).toEqual(falsy)
+        jest.runTimersToTime(100)
     })
 
-    it('should stop interval after 50 ms (inclusive)', () => {
+    it('should call steps in sequence', () => {
+        let counter = 0
+        const stack = []
+        const timer = setInterval(() => ++counter, 1)
+        const step1 = jest.fn(() => stack.push(counter))
+        const step2 = jest.fn(() => stack.push(counter))
+        const step3 = jest.fn(() => stack.push(counter))
+        const step4 = jest.fn(() => stack.push(counter))
+
+        const request = aframe
+            .waitTimeout(step1, 10)
+            .waitTimeout(step2, 10)
+            .waitTimeout(step3, 20)
+            .waitTimeout(step4, 30)
+
+        jest.runTimersToTime(200)
+        expect(stack).toEqual([10, 20, 40, 70])
+    })
+
+    it('should stop after falsy output', () => {
+        let limit = 2
+        const callback = jest.fn(() => --limit > 0)
+        aframe
+            .waitTimeout(callback, 10)
+            .waitTimeout(callback, 10)
+            .waitTimeout(callback, 20)
+            .waitTimeout(callback, 30)
+
+        jest.runTimersToTime(200)
+        expect(callback).toHaveBeenCalledTimes(2)
+    })
+
+    it('should stop after 20 ms', () => {
         const callback = jest.fn()
-        const request = aframe.setInterval(callback, 10)
-        setTimeout(() => aframe.clear(request), 50)
+        const request = aframe
+            .waitTimeout(callback, 10)
+            .waitTimeout(callback, 10)
+            .waitTimeout(callback, 20)
+            .waitTimeout(callback, 30)
 
-        jest.runTimersToTime(150) // 100 frames more
-        expect(callback).toHaveBeenCalledTimes(4)
+        setTimeout(() => aframe.clear(request), 20)
+        jest.runTimersToTime(200)
+        expect(callback).toHaveBeenCalledTimes(2)
     })
 
-    it('should assign -1 to the request id', () => {
-        const request = { id: 10 }
-        aframe.clear(request)
-        expect(request).toEqual({ id: -1 })
+    it('should handle extra parameters', () => {
+        const callback = jest.fn()
+        aframe
+            .waitTimeout(callback, 10, 'a', 'b')
+            .waitTimeout(callback, 20, 'c', 'd')
+
+        jest.runTimersToTime(200)
+        const { calls } = callback.mock
+        expect(calls).toEqual([['a', 'b'], ['c', 'd']])
     })
 })
 
